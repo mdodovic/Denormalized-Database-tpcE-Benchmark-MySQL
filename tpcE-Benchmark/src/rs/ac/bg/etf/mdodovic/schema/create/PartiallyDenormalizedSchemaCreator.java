68097c34314bc18ce88f6c123906474219a73fb0
mdodovic <matija.dodovic@gmail.com>
2022-05-31 10:13:03 +0200

    raise indexes on PartialDT

--- tpcE-Benchmark/src/rs/ac/bg/etf/mdodovic/schema/create/PartiallyDenormalizedSchemaCreator.java
+++ tpcE-Benchmark/src/rs/ac/bg/etf/mdodovic/schema/create/PartiallyDenormalizedSchemaCreator.java
@@ -1,6 +1,8 @@
 package rs.ac.bg.etf.mdodovic.schema.create;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -96,6 +98,92 @@ public class PartiallyDenormalizedSchemaCreator {
 
 	}	
 
+	public static void raiseIndexes(Connection connection) throws SQLException {
+
+		String indexName;
+		String tableName;
+		String columnName;
+		
+		String hasIndexOnTableQuery = "	SELECT * "
+									+ "    FROM INFORMATION_SCHEMA.STATISTICS "
+									+ "	   WHERE INDEX_SCHEMA = ? "
+									+ "		  AND TABLE_NAME = ? "
+									+ "		  AND INDEX_NAME = ? ";
+
+		String dropIndexPattern = "DROP INDEX #1# ON tpce_mysql.#2#";
+		String dropIndexQuery;
+		String createIndexPattern = "CREATE INDEX #1# ON tpce_mysql.#2# (#3#) USING BTREE";
+		String createIndexQuery;
+
+		Statement stmt;
+		PreparedStatement pStmt;
+		ResultSet rs;
+/*		
+		tableName = "DTT2T3T8";
+		indexName = "DTT2T3T8_Index_DT_CA_C_ID";
+		columnName = "DT_CA_C_ID";
+						
+		pStmt = connection.prepareStatement(hasIndexOnTableQuery);  
+		pStmt.setString(1, "tpce_mysql");
+		pStmt.setString(2, tableName);
+		pStmt.setString(3, indexName);
+		
+		rs = pStmt.executeQuery();
+		if(rs.next()) {
+
+			dropIndexQuery = dropIndexPattern.replace("#2#", tableName);
+			dropIndexQuery = dropIndexQuery.replace("#1#", indexName);
+
+			stmt = connection.createStatement();
+			stmt.executeUpdate(dropIndexQuery);
+			
+		} 
+				
+		createIndexQuery = createIndexPattern.replace("#2#", tableName);
+		createIndexQuery = createIndexQuery.replace("#1#", indexName);
+		createIndexQuery = createIndexQuery.replace("#3#", columnName);
+		
+		stmt = connection.createStatement();
+
+		stmt.execute(createIndexQuery);
+
+		System.out.println("Index: " + indexName + " successfully created");
+
+		
+		tableName = "DTT2T3T8";
+		indexName = "DTT2T3T8_Index_DT_HS_S_SYMB__DT_CA_ID";
+		columnName = "DT_HS_S_SYMB, DT_CA_ID";
+						
+		pStmt = connection.prepareStatement(hasIndexOnTableQuery);  
+		pStmt.setString(1, "tpce_mysql");
+		pStmt.setString(2, tableName);
+		pStmt.setString(3, indexName);
+		
+		rs = pStmt.executeQuery();
+		if(rs.next()) {
+
+			dropIndexQuery = dropIndexPattern.replace("#2#", tableName);
+			dropIndexQuery = dropIndexQuery.replace("#1#", indexName);
+
+			stmt = connection.createStatement();
+			stmt.executeUpdate(dropIndexQuery);
+			
+		} 
+				
+		createIndexQuery = createIndexPattern.replace("#2#", tableName);
+		createIndexQuery = createIndexQuery.replace("#1#", indexName);
+		createIndexQuery = createIndexQuery.replace("#3#", columnName);
+		
+		stmt = connection.createStatement();
+
+		stmt.execute(createIndexQuery);
+
+		System.out.println("Index: " + indexName + " successfully created");
+*/
+		System.out.println("------------------------------------------------------------");
+
+		
+	}
 
 
 	
