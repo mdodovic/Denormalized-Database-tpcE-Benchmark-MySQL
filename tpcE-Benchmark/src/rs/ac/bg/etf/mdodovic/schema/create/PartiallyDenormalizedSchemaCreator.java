ad2ca6c7887fb3d9dc6d33eb8539a5bb17399aef
mdodovic <matija.dodovic@gmail.com>
2022-05-31 10:10:59 +0200

    drop PartialDT and raise foreign keys on PartialDT

--- tpcE-Benchmark/src/rs/ac/bg/etf/mdodovic/schema/create/PartiallyDenormalizedSchemaCreator.java
+++ tpcE-Benchmark/src/rs/ac/bg/etf/mdodovic/schema/create/PartiallyDenormalizedSchemaCreator.java
@@ -34,6 +34,69 @@ public class PartiallyDenormalizedSchemaCreator {
 		System.out.println("------------------------------------------------------------");
 		
 	}
+	
+	public static void dropFullyDenormalizedDatabaseChema(Connection connection) throws SQLException {
+		
+		String dropQueryPattern = "DROP TABLE IF EXISTS ### CASCADE;";
+
+		String dropQuery;
+		Statement stmt;
+		String tableName;
+
+		tableName = Tables.partiallyDenormalizedTableT2T3T8F2;
+		dropQuery = dropQueryPattern.replace("###",  tableName);
+	
+		stmt = connection.createStatement();
+		stmt.executeUpdate(dropQuery);
+
+		System.out.println("Table: " + tableName + " successfully deleted");
+
+		tableName = Tables.partiallyDenormalizedTableT8F6;
+		dropQuery = dropQueryPattern.replace("###",  tableName);
+	
+		stmt = connection.createStatement();
+		stmt.executeUpdate(dropQuery);
+
+		System.out.println("Table: " + tableName + " successfully deleted");
+
+		
+		System.out.println("------------------------------------------------------------");
+
+	}
+	
+	public static void raiseForeignKeyConstraints(Connection connection) throws SQLException {
+		
+		String foreignKeyConstraintsQuery;
+		Statement stmt; 
+		String tableName;
+
+		tableName = Tables.partiallyDenormalizedTableT2T3T8F2;
+		
+		foreignKeyConstraintsQuery = createForeignKeyConstraintsTableQuerry(tableName);
+		if(foreignKeyConstraintsQuery != null) {
+			stmt = connection.createStatement();
+			stmt.executeUpdate(foreignKeyConstraintsQuery);
+			
+			System.out.println("Foreign key(s) on table " + tableName + " successfully raised");
+
+		}
+
+		tableName = Tables.partiallyDenormalizedTableT8F6;
+		
+		foreignKeyConstraintsQuery = createForeignKeyConstraintsTableQuerry(tableName);
+		if(foreignKeyConstraintsQuery != null) {
+			stmt = connection.createStatement();
+			stmt.executeUpdate(foreignKeyConstraintsQuery);
+			
+			System.out.println("Foreign key(s) on table " + tableName + " successfully raised");
+
+		}
+
+		System.out.println("------------------------------------------------------------");
+
+	}	
+
+
 
 	
 }
