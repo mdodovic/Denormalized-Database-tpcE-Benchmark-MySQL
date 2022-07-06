291995c3f68df8e413b34bfa3e187c4c68271fc3
mdodovic <matija.dodovic@gmail.com>
2022-07-06 16:48:36 +0200

    T2F1 count test

---
+++ mysql-queries/tpcE-modified-agregate/COUNT(T2F1)_No_Conditions_No_GroupBy_NT.sql
@@ -0,0 +1,9 @@
+SELECT COUNT(NT.CA_ID)
+	FROM (
+		SELECT CA_ID, CA_BAL
+			FROM tpce_mysql.CUSTOMER_ACCOUNT 
+				LEFT OUTER JOIN (
+							tpce_mysql.HOLDING_SUMMARY 
+								INNER JOIN tpce_mysql.LAST_TRADE ON (LT_S_SYMB = HS_S_SYMB)
+							) ON HS_CA_ID = CA_ID
+		) NT
---
+++ mysql-queries/tpcE-modified-agregate/COUNT(T2F1)_No_Conditions_No_GroupBy_PartiallyDT.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(PDT.CA_ID) 
+	FROM (
+		SELECT tpce_mysql.DTT2T3T8F2.DT_CA_ID AS CA_ID, DT_CA_BAL AS CA_BAL
+			FROM tpce_mysql.DTT2T3T8F2 inner join tpce_mysql.DTT8F6 ON tpce_mysql.DTT2T3T8F2.DT_CA_ID = tpce_mysql.DTT8F6.DT_CA_ID  
+			WHERE DT_HS_S_SYMB IS NOT NULL
+	) PDT
